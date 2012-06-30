6ccde874c0882eb5fa9fd68ffa145ad7a4b798d6
Javier Manzano <javier.manzano22@gmail.com>
2012-06-30 19:27:02 +0200

    Finished example!

--- gen/es/jmanzano/jelly/notifications/R.java
+++ gen/es/jmanzano/jelly/notifications/R.java
@@ -21,7 +21,11 @@ public final class R {
         public static final int jellybean=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int button1=0x7f080000;
+        public static final int button2=0x7f080001;
+        public static final int button3=0x7f080002;
+        public static final int button4=0x7f080003;
+        public static final int menu_settings=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -31,8 +35,12 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int big_picture_notification=0x7f050005;
+        public static final int big_text_notification=0x7f050006;
         public static final int hello_world=0x7f050001;
+        public static final int inbox_notification=0x7f050007;
         public static final int menu_settings=0x7f050002;
+        public static final int simple_notification=0x7f050004;
         public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
--- src/es/jmanzano/jelly/notifications/MainActivity.java
+++ src/es/jmanzano/jelly/notifications/MainActivity.java
@@ -10,65 +10,126 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
-import es.jmanzano.jelly.notifications.R;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        String ns = Context.NOTIFICATION_SERVICE;
-        NotificationManager notificationManager = (NotificationManager) getSystemService(ns);
-        
-        int icon = android.R.drawable.ic_menu_camera;
-        CharSequence tickerText = "Hello";
-        long when = System.currentTimeMillis();
-        //Notification notification = new Notification(icon, tickerText, when);
+	private NotificationManager notificationManager;
 
-        Builder build = new Notification.Builder(this)
-	        .setContentTitle("New mail from me")
-	        .setContentText("subject")
-	        .setTicker(tickerText)
-	        .setSmallIcon(R.drawable.ic_action_search)
-	        .addAction(
-	        			android.R.drawable.ic_btn_speak_now,
-	        			"Speak!",
-	        			PendingIntent.getActivity(getApplicationContext(), 0, getIntent(), 0, null)
-	        			)
-	        .addAction(
-	        			android.R.drawable.ic_dialog_map,
-	        			"Maps",
-	        			PendingIntent.getActivity(getApplicationContext(), 0, getIntent(), 0, null)
-	        			)
-	        .addAction(
-    					android.R.drawable.ic_dialog_info,
-    					"Info",
-    					PendingIntent.getActivity(getApplicationContext(), 0, getIntent(), 0, null)
-    					)
-	        ;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		notificationManager = getNotificationManager();
+	}
 
-        Notification notification = new Notification
-        	.BigPictureStyle(build)
-        	.bigPicture(BitmapFactory.decodeResource(getResources(), R.drawable.jellybean))
-        	.build();
-        
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        
-        CharSequence contentTitle = "Notification";
-        CharSequence contentText = "Hello world!";
-        PendingIntent activity = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-        notificationManager.notify(0, notification);
-        
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
-    }
+	private NotificationManager getNotificationManager() {
+		return (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	/**
+	 * Push a simple notification (old way)
+	 */
+	private void pushSimpleNotification() {
+		Notification notification = new Notification(
+				android.R.drawable.ic_menu_camera, "Hello camera!",
+				System.currentTimeMillis());
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
+		notification.number += 1;
 
-    
+		Intent intent = new Intent(this, MainActivity.class);
+		PendingIntent activity = PendingIntent.getActivity(this, 0, intent, 0);
+		notification.setLatestEventInfo(this, "title", "content", activity);
+		notificationManager.notify(0, notification);
+	}
+
+	/**
+	 * Big picture notification!
+	 */
+	public void pushBigPictureNotifications() {
+		Builder build = new Notification.Builder(this)
+				.setContentTitle("New mail from me")
+				.setContentText("subject")
+				.setTicker("New email with photo")
+				.setSmallIcon(R.drawable.ic_action_search)
+				.addAction(
+						android.R.drawable.ic_btn_speak_now,
+						"Speak!",
+						PendingIntent.getActivity(getApplicationContext(), 0,
+								getIntent(), 0, null))
+				.addAction(
+						android.R.drawable.ic_dialog_map,
+						"Maps",
+						PendingIntent.getActivity(getApplicationContext(), 0,
+								getIntent(), 0, null))
+				.addAction(
+						android.R.drawable.ic_dialog_info,
+						"Info",
+						PendingIntent.getActivity(getApplicationContext(), 0,
+								getIntent(), 0, null));
+
+		Notification notification = new Notification.BigPictureStyle(build)
+				.bigPicture(
+						BitmapFactory.decodeResource(getResources(),
+								R.drawable.jellybean)).build();
+
+		notificationManager.notify(0, notification);
+	}
+
+	/**
+	 * Big notifications
+	 */
+	public void pushBigTextNotification() {
+		Builder builder = new Notification.Builder(this)
+				.setContentTitle("New mail from me").setContentText("subject")
+				.setSmallIcon(android.R.drawable.ic_menu_agenda);
+		Notification notification = new Notification.BigTextStyle(builder)
+				.bigText("Very long stringgggggg!!!! It will fill the space of more than 1 line! It's amazing how Jelly Bean notifications work!")
+				.build();
+		notificationManager.notify(0, notification);
+	}
+
+	/**
+	 * Inbox notifications
+	 */
+	private void pushInboxNotifications() {
+		Builder builder = new Notification.Builder(this)
+				.setContentTitle("5 New mails from me")
+				.setContentText("subject")
+				.setSmallIcon(android.R.drawable.ic_menu_agenda);
+
+		Notification notification = new Notification.InboxStyle(builder)
+				.addLine("line1")
+				.addLine("line2")
+				.addLine("line3")
+				.setSummaryText("+3 more")
+				.build();
+		notificationManager.notify(0, notification);
+	}
+
+	public void onClick(View v) {
+		int id = v.getId();
+		switch (id) {
+		case R.id.button1:
+			pushSimpleNotification();
+			break;
+		case R.id.button2:
+			pushBigPictureNotifications();
+			break;
+		case R.id.button3:
+			pushBigTextNotification();
+			break;
+		case R.id.button4:
+			pushInboxNotifications();
+			break;
+		}
+
+	}
+
 }
